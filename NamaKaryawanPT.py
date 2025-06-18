ad0850c37ee0abbcca389681f2d5734e642c7876
RIZALD6969 <muhamadhaikal@student.uns.ac.id>
2025-06-18 10:22:40 +0700

    Update NamaKaryawanPT.py

--- NamaKaryawanPT.py
+++ NamaKaryawanPT.py
@@ -57,7 +57,7 @@ class KepalaDivisi(Karyawan):
         self.nama_divisi = nama_divisi
 
     def gaji_pokok(self):
-        return 8000000
+        return 9000000
 
     def total_gaji(self):
         return self.gaji_pokok() + self.tunjangan() + self.insentif()
@@ -69,7 +69,7 @@ class Staff(Karyawan):
         self.nama_divisi = nama_divisi
 
     def gaji_pokok(self):
-        return 6000000
+        return 7000000
 
     def total_gaji(self):
         return self.gaji_pokok() + self.tunjangan() + self.insentif()
